fb485b357bc25a5ffead03b56f29e87b3744d1cc
tylerasa <sarpongasaresylvester@gmail.com>
2024-03-13 00:00:17 +0000

    feat: getting the line and file name from the uri

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,47 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // Our implementation of a UriHandler.
 class MyUriHandler implements vscode.UriHandler {
-	// This function will get run when something redirects to VS Code
-	// with your extension id as the authority.
-	handleUri(uri: vscode.Uri): vscode.ProviderResult<void> {
-		let message = "Handled a Uri!";
-		if (uri.query) {
-			message += ` It came with this query: ${uri.query}`;
-		}
-		vscode.window.showInformationMessage(message);
-	}
+  // This function will get run when something redirects to VS Code
+  // with your extension id as the authority.
+
+  // vscode://tylerasa.talaria-server?file=app.tsx&line=30
+  handleUri(uri: vscode.Uri): vscode.ProviderResult<void> {
+    let message = "Handled a Uri!";
+    if (uri.query) {
+      const params = new URLSearchParams(uri.query);
+      const fileValue = params.get("file");
+      const lineValue = params.get("line");
+
+      console.log("File:", fileValue);
+      console.log("Line:", lineValue);
+      message += ` It came with this file: ${fileValue} and line: ${lineValue}`;
+    }
+    vscode.window.showInformationMessage(message);
+  }
 }
 
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "talaria-server.start",
+    async () => {
+      // Create our new UriHandler
+      const uriHandler = new MyUriHandler();
 
-	const disposable = vscode.commands.registerCommand('talaria-server.start', async () => {
-		// Create our new UriHandler
-		const uriHandler = new MyUriHandler();
+      // And register it with VS Code. You can only register a single UriHandler for your extension.
+      context.subscriptions.push(vscode.window.registerUriHandler(uriHandler));
 
-		// And register it with VS Code. You can only register a single UriHandler for your extension.
-		context.subscriptions.push(vscode.window.registerUriHandler(uriHandler));
+      // You don't have to get the Uri from the `vscode.env.asExternalUri` API but it will add a query
+      // parameter (ex: "windowId%3D14") that will help VS Code decide which window to redirect to.
+      // If this query parameter isn't specified, VS Code will pick the last windows that was focused.
+      const uri = await vscode.env.asExternalUri(
+        vscode.Uri.parse(`${vscode.env.uriScheme}://tylerasa.talaria-server`)
+      );
+      vscode.window.showInformationMessage(
+        `Starting to handle Uris. Open ${uri} in your browser to test.`
+      );
+    }
+  );
 
-		// You don't have to get the Uri from the `vscode.env.asExternalUri` API but it will add a query
-		// parameter (ex: "windowId%3D14") that will help VS Code decide which window to redirect to.
-		// If this query parameter isn't specified, VS Code will pick the last windows that was focused.
-		const uri = await vscode.env.asExternalUri(vscode.Uri.parse(`${vscode.env.uriScheme}://tylerasa.talaria-server`));
-		vscode.window.showInformationMessage(`Starting to handle Uris. Open ${uri} in your browser to test.`);
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
